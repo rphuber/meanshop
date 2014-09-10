eb6e071ffd25caa59615d46a1a149d73d9bdcd8f
Ryan Huber <ryan.huber@mac.com>
2014-09-10 03:23:26 -0400

    updates title of the page

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', ngApp: 'meanstore' });
+  res.render('index', { title: 'The Mean Store', ngApp: 'meanstore' });
 };
