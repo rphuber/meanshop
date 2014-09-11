c87d8052151de0dca184e17bedb596621119b091
Ryan Huber <ryan.huber@mac.com>
2014-09-10 20:58:52 -0400

    fixes bug in removeItemFromCart function

--- public/js/meanstore/meanstoreControl.js
+++ public/js/meanstore/meanstoreControl.js
@@ -19,7 +19,7 @@ angular.module('meanstore')
     };
 
     $scope.removeItemFromCart = function(item) {
-      cartItemsService.delet(item);
+      cartItemsService.delete(item);
       $location.path('/meanstore/cartList');
     };
 
